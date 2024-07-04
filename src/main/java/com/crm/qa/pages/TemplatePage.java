e6b353559d28258f209e1283297f3506b371ca4d
Shubhamsinh1995 <baiyasshubham@gmail.com>
2024-07-04 19:49:41 +0530

    new TemplatePage Added

---
+++ src/main/java/com/crm/qa/pages/TemplatePage.java
@@ -0,0 +1,11 @@
+package com.crm.qa.pages;
+
+import com.crm.qa.base.TestBase;
+
+public class TemplatePage extends TestBase {
+
+	public TemplatePage() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
